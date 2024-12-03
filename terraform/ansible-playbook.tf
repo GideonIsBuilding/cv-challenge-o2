4a0fbaad4eb3b4ca225747ec0b1504661ea047ac
Aleonogwe Gideon <galosikhena@gmail.com>
2024-12-03 15:58:24 +0100

    made some edits

--- terraform/ansible-playbook.tf
+++ terraform/ansible-playbook.tf
@@ -1,9 +1,11 @@
 resource "null_resource" "ansible" {
     provisioner "local-exec" {
-        command = <<EOT
-        ansible-playbook -i ../ansible/inventory ../ansible/playbook.yml --extra-vars "@../ansible/vars/docker_hub.yml"
-        EOT
+        command = "ansible-playbook -vvv -T 180 -i /home/gideon/cv-challenge-o2/ansible/inventory /home/gideon/cv-challenge-o2/ansible/playbook.yml --extra-vars '@/home/gideon/cv-challenge-o2/ansible/vars/docker_hub.yml'"
     }
 
     depends_on = [ aws_instance.app ]
 }
+
+# command = "ansible-playbook -i ${path.module}/../ansible/inventory ${path.module}/../ansible/playbook.yml --extra-vars '@${path.module}/../ansible/vars/docker_hub.yml'"
+
+# ansible-playbook -i inventory playbook.yml --extra-vars @vars/docker_hub.yml
--- terraform/inventory.tpl
+++ terraform/inventory.tpl
@@ -1,2 +1,2 @@
 [app]
-54.90.144.248 ansible_user=ubuntu ansible_ssh_private_key_file=~/home/gideon/Downloads/nginx.pem
+54.90.144.248 ansible_user=ubuntu ansible_ssh_private_key_file=/home/gideon/Downloads/nginix.pem ansible_ssh_common_args='-o StrictHostKeyChecking=no'
